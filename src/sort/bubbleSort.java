bdd0ec91306bc4b5c80e997a977bbd1b2aab9815
BCX <1184127373@qq.com>
2020-04-12 23:53:47 +0800

    aaa

---
+++ src/sort/bubbleSort.java
@@ -0,0 +1,27 @@
+package sort;
+
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void bubbleSort(int []array){
+        //冒泡排序的思想：在无序区间里比较相邻两个元素，按照一定的顺序进行交换，持续这个过程直到数组有序
+        for (int bound=0;bound<array.length-1;bound++){
+            //默认升序的话，找最小，从后往前遍历
+            //cur>bound为止，因为跑bound趟后，bound前面的元素已经排好序了，不需要再比较
+            for (int cur=array.length-1;cur>bound;cur--){
+                //比较相邻两个元素的大小关系
+                if (array[cur-1]>array[cur]){
+                    int temp=array[cur];
+                    array[cur]=array[cur-1];
+                    array[cur-1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[]array={9,7,1,6,0,3,5,3,7};
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+}
---
+++ src/sort/heapSort.java
@@ -0,0 +1,64 @@
+package sort;
+
+import java.util.Arrays;
+
+public class heapSort {
+    public static void heapSort(int[] array) {
+        //创建一个大堆
+        creatHeap(array);
+        //因为比较到最后面就只剩一个元素了，不需要再进行比较，所以是length-1；
+        //循环把队顶元素和堆尾元素交换，再删除堆尾元素，再调整堆
+        //该元素逻辑上是删除，实际是在数组的末尾
+        for (int i = 0; i < array.length - 1; i++) {
+            int heapsize=array.length-1;//堆的元素个数
+            //因为堆的长度随循环一发生变化，所以每次交换的位置应该是更新后的堆的最后一个元素位置
+            //堆的最后一个元素下标就是length-1-i；
+            swap(array,0,heapsize-i);
+            //再把堆向下调整，重新进入下一次循环
+            //交换完成后要把最后一个元素从堆中删除，数组中元素个数为length-i；
+            // 那么删除后此时堆的元素个数就是length-i-1（实现删除操作）
+            shiftDown(array,array.length-i-1,0);
+        }
+    }
+    //交换堆顶和堆尾元素
+    private static void swap(int[] array, int i, int j) {
+        int temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+
+    }
+    //向下调整
+     public static void shiftDown(int []array,int size,int index){
+        int parent=index;
+        int left=parent*2+1;
+        while (left<size){
+            int right=parent*2+2;
+            if (right<size){
+                //左右子树都存在的情况下找最大去和父节点进行比较
+                if (array[right]>=array[left]){
+                    left=right;
+                }
+            }
+            if (array[left]>array[parent]){
+                swap(array,left,parent);
+            }
+            //更新parent的位置
+            parent=left;
+            left=parent*2+1;
+        }
+     }
+
+    private static void creatHeap(int[]array) {
+        //升序建大堆，降序建小堆
+        //向下调整从后往前遍历，向上调整从前往后遍历
+        //从最后一个叶子节点的父节点开始
+        for (int i=(array.length-1-1)/2;i>=0;i--){
+            shiftDown(array,array.length,i);
+        }
+    }
+    public static void main(String[] args){
+        int [] array={2,1,2,0,6,9,5,7};
+        heapSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
