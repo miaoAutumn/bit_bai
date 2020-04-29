2d58953b2c667a2cf3e6c0e28f47ead50d1923cc
BCX <1184127373@qq.com>
2020-04-29 23:56:48 +0800

    aaa

--- src/container/StackQueueInterview.java
+++ src/container/StackQueueInterview.java
@@ -51,5 +51,74 @@ public class StackQueueInterview {
     }
 
 
+    //比较含有退格的字符串 力扣844
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+        }
+    private String build(String s) {
+        //因为处理S,T字符串的操作一样,所以放在一个单独的辅助方法中
+        char [] charA = s.toCharArray();
+        Stack<Character> A = new Stack<>();
+        //循环将S中的元素按要求入栈
+        for (int i = 0;i < charA.length;i++){
+            if (charA[i] != '#'){
+                A.push(charA[i]);
+            }
+            //考虑A##B的情况,防止空栈出栈顶元素
+            else if (A.isEmpty()){
+                continue;
+            }
+            else {
+                A.pop();
+            }
+        }
+     return String.valueOf(A);
+    }
+
+
+    //棒球比赛 力扣682
+    public int calPoints(String[] ops) {
+   //只有C一个操作是删除操作,其余操作都可看成是一个数的累计过程
+   // 用栈来存储有效元素,模拟积分过程
+        Stack<Integer> stack = new Stack<>();
+        //循环遍历字符串入栈,按计分原则入栈
+        for (int i = 0;i<ops.length;i++){
+            if (ops[i].equals( "D")){
+             //前一轮有效得分就是栈顶元素
+             Integer temp  = stack.peek();
+             Integer value = temp *2;
+             stack.push(value);
+            }
+           else if (ops[i].equals("+")){
+            //取前两个栈顶元素,记录第一个的值出栈,再记录第二个值,再将第一个入栈,再将新元素入栈
+                int temp = stack.pop();
+                int temp2 = stack.peek();
+                int value = temp + temp2;
+                stack.push(temp);
+                stack.push(value);
+            }
+           else if (ops[i].equals("C")){
+               //将栈顶元素出栈
+                stack.pop();
+            }
+            else {
+                stack.push(Integer.valueOf(ops[i]));
+            }
+
+        }
+        int sum = 0;
+       for (int i : stack){
+          sum+=i;
+       }
+       return sum;
+    }
+    
+    public static void main(String[] args){
+        String [] test = {"5","2","C","D","+"};
+        StackQueueInterview stackQueueInterview = new StackQueueInterview();
+        System.out.println( stackQueueInterview.calPoints(test));
+
+    }
+    
 
 }
